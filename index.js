bd9e041f783bfeee74e114f63b14cb1aa80479fe
musaakramsaleh <saleh01715972188@gmail.com>
2024-04-28 11:31:59 +0600

    ok

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.get('/',(req,res)=>{
     res.send('Painting is running')
 })
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zuwbcyf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +30,12 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    app.post('/addproduct',async(req,res)=>{
+      const product = req.body
+      console.log(product)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
