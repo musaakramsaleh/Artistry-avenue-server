c6a8acbf29530debc86194873faefe93949f4241
musaakramsaleh <saleh01715972188@gmail.com>
2024-04-28 12:20:30 +0600

    ok

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.get('/',(req,res)=>{
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zuwbcyf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -30,22 +31,33 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("painting");
+    const userCollection = database.collection("paints");
+
+   
     app.post('/addproduct',async(req,res)=>{
-      const product = req.body
-      console.log(product)
+        const product = req.body;
+        console.log("new user",product)
+        const result = await userCollection.insertOne(product);
+        res.send(result)
+
     })
-
-
+   
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get('/',(req,res)=>{
+    res.send("Simple Crud is running")
+})
+
 app.listen(port,()=>{
-    console.log(`Painting server is running on port ${port}`)
+    console.log(`App is running on port ${port}`)
 })
